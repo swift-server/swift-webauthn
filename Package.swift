aa3cceafb6d75f986387ae1db8b3f39d1d1711d8
Tim <0xtimc@gmail.com>
2022-07-20 17:06:13 +0100

    Add headers

--- Package.swift
+++ Package.swift
@@ -1,4 +1,18 @@
 // swift-tools-version:5.6
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the WebAuthn Swift open source project
+//
+// Copyright (c) 2022 the WebAuthn Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of WebAuthn Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import PackageDescription
 
 let package = Package(
