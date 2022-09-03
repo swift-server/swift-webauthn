7eb0eb7890907564f2d269fe4a89ca8c5052eacd
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2022-09-03 13:34:33 +0100

    Create Library Product in Package.swift

--- Package.swift
+++ Package.swift
@@ -16,12 +16,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "PasskeyDemo",
+    name: "WebAuthn",
     platforms: [
-       .macOS(.v12)
+        .macOS(.v12)
+    ],
+    products: [
+        .library(name: "WebAuthn", targets: ["WebAuthn"]),
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/unrelentingtech/SwiftCBOR.git", from: "0.4.5"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
