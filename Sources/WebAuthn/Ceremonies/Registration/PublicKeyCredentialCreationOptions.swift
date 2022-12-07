47270ad5014505bac9a435cc19cfd1fbea132c07
Marius Seufzer <marius.seufzer@protonmail.com>
2022-12-07 22:42:11 +0100

    wip

--- Sources/WebAuthn/Ceremonies/Registration/PublicKeyCredentialCreationOptions.swift
+++ Sources/WebAuthn/Ceremonies/Registration/PublicKeyCredentialCreationOptions.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/// See §5.4. https://www.w3.org/TR/webauthn/#dictionary-makecredentialoptions
+/// From §5.4. (https://www.w3.org/TR/webauthn/#dictionary-makecredentialoptions)
 /// Contains a PublicKeyCredentialCreationOptions object specifying the desired attributes of the
 /// to-be-created public key credential.
 public struct PublicKeyCredentialCreationOptions: Codable {
@@ -10,4 +10,36 @@ public struct PublicKeyCredentialCreationOptions: Codable {
     public let relyingParty: PublicKeyCredentialRpEntity
     public let publicKeyCredentialParameters: [PublicKeyCredentialParameters]
     public let timeout: TimeInterval
+}
+
+// MARK: - Credential parameters
+
+public struct PublicKeyCredentialParameters: Codable {
+    let type: String
+    let algorithm: COSEAlgorithmIdentifier
+
+    public static var supported: [Self] {
+        COSEAlgorithmIdentifier.allCases.map {
+            PublicKeyCredentialParameters.init(type: "public-key", algorithm: $0)
+        }
+    }
+}
+
+// MARK: - Credential entities
+
+/// From §5.4.2 (https://www.w3.org/TR/webauthn/#sctn-rp-credential-params).
+/// The PublicKeyCredentialRpEntity dictionary is used to supply additional Relying Party attributes when
+/// creating a new credential.
+public struct PublicKeyCredentialRpEntity: Codable {
+    public let name: String
+    public let id: String
+}
+
+/// From §5.4.3 (https://www.w3.org/TR/webauthn/#dictionary-user-credential-params)
+/// The PublicKeyCredentialUserEntity dictionary is used to supply additional user account attributes when
+/// creating a new credential.
+public struct PublicKeyCredentialUserEntity: Codable {
+    public let name: String
+    public let id: String
+    public let displayName: String
 }
