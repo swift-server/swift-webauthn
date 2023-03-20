1e5f67e4d6c991b0263957a7d46d48f0c8f19084
Marius Seufzer <marius.seufzer@protonmail.com>
2023-03-20 09:47:27 +0100

    add comments

--- Sources/WebAuthn/Ceremonies/Authentication/VerifiedAuthentication.swift
+++ Sources/WebAuthn/Ceremonies/Authentication/VerifiedAuthentication.swift
@@ -7,8 +7,13 @@ public struct VerifiedAuthentication {
         case multiDevice = "multi_device"
     }
 
+    /// The credential id associated with the public key
     public let credentialID: URLEncodedBase64
+    /// The updated sign count after the authentication ceremony
     public let newSignCount: UInt32
+    /// Whether the authenticator is a single- or multi-device authenticator. This value is determined after
+    /// registration and will not change afterwards.
     public let credentialDeviceType: CredentialDeviceType
+    /// Whether the authenticator is known to be backed up currently
     public let credentialBackedUp: Bool
 }
