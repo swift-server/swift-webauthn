95ce09430d13852db971db56ae0b55d1911db2ee
Marius Seufzer <44228394+marius-se@users.noreply.github.com>
2024-02-13 12:02:40 +1300

    Update dependencies (#35)
    * update swift-crypto and remove swift-certificates
    
    * add trailing comma

--- Package.swift
+++ Package.swift
@@ -25,9 +25,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/unrelentingtech/SwiftCBOR.git", from: "0.4.5"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "2.0.0" ..< "4.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-certificates.git", from: "0.3.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0")
     ],
     targets: [
@@ -38,7 +37,6 @@ let package = Package(
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "_CryptoExtras", package: "swift-crypto"),
                 .product(name: "Logging", package: "swift-log"),
-                .product(name: "X509", package: "swift-certificates")
             ]
         ),
         .testTarget(name: "WebAuthnTests", dependencies: [
