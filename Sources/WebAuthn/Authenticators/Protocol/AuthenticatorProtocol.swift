60e6815eb2a1eeba79463997bf4709fc2037a308
Dimitri Bouniol <dimitribouniol@mochidev.com>
2024-02-24 03:12:15 -0800

    Added new AuthenticatorProtocol for modular authenticators

---
+++ Sources/WebAuthn/Authenticators/Protocol/AuthenticatorProtocol.swift
@@ -0,0 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the WebAuthn Swift open source project
+//
+// Copyright (c) 2024 the WebAuthn Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of WebAuthn Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+public protocol AuthenticatorProtocol<CredentialSource> {
+    associatedtype CredentialSource: AuthenticatorCredentialSourceProtocol
+}
